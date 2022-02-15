95ade644589968be60ba092e77476c1be2fcd114
Pankhuri <36470694+Pankhuri1999@users.noreply.github.com>
2022-02-15 12:21:12 -0500

    Create Solution.cpp

---
+++ Feb15,22/Sqrt(x)/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long int l=1, r=x, mid;
+        while(l<=r)
+        {
+            mid = (l+r)/2;
+            if (mid*mid==x)
+            {
+                return mid;
+            }
+            else if (mid*mid>x)
+            {
+                r = mid-1;
+            }
+            else if (mid*mid<x)
+            {
+                l = mid+1;
+            }
+        }
+        return r;
+    }
+};
